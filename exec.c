12428c2c8fd62a7b7a6e8e412c7ac8e149aba038
SCCSMARTCODE <sccsmart247@gmail.com>
2023-09-19 11:34:28 -0700

    This is a very challenging project

---
+++ exec.c
@@ -0,0 +1,163 @@
+#include "shell.h"
+
+/**
+ * parser - Tokenize a buffer into an array of strings.
+ * @buffer: The input buffer to tokenize.
+ *
+ * Return: An array of strings containing the tokens or NULL on failure.
+ */
+char **parser(char *buffer)
+{
+	int i = 0, x = 0;
+	char *token, **tokenized, *delim = " \n  ", *buf;
+
+	buf = my_strdup(buffer);
+	token = sstrtok(buf, delim);
+	while (token)
+	{
+		x++;
+		token = sstrtok(NULL, delim);
+	}
+	tokenized = malloc(sizeof(char *) * (x + 1));
+	if (tokenized == NULL)
+	{
+		return (NULL);
+	}
+	token = sstrtok(buffer, delim);
+
+	while (token)
+	{
+		tokenized[i] = malloc(sizeof(char) * (my_strlen(token) + 1));
+		tokenized[i] = my_strdup(token);
+		token = sstrtok(NULL, delim);
+		i++;
+	}
+	tokenized[i] = NULL;
+	free(buf);
+
+	return (tokenized);
+}
+
+/**
+ * process - Execute a command.
+ * @info: A pointer to the hsh structure.
+ */
+void process(hsh *info)
+{
+	pid_t child_id;
+	int status;
+
+	child_id = fork();
+	if (child_id == 0)
+	{
+		if (execve(info->path, info->args, _env(info)) == -1)
+		{
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
+}
+
+/**
+ * executor - Execute commands in a loop.
+ * @info: A pointer to the hsh structure.
+ * @argv: An array of argument strings.
+ *
+ * Return: The exit status of the last executed command or -1 on failure.
+ */
+int executor(hsh *info, char **argv)
+{
+	char *buffer = NULL;
+	size_t n = 0;
+	ssize_t read_num;
+	int i = 0, check;
+
+	while (1)
+	{
+		if (info->interact)
+		{
+			prompt();
+			fflush(stdout);
+		}
+		read_num = getline(&buffer, &n, stdin);
+		if (read_num == -1)
+		{
+			free(buffer);
+			return (-1);
+		}
+		info->av = argv;
+		info->args = parser(buffer);
+		if (info->args[i] == NULL)
+		{
+			free_args(info->args);
+			continue;
+		}
+		info->arg = info->args[0];
+		check = builtin(info);
+		if (check == -1)
+		{
+			info->path = path_tok(info);
+			if (info->path == NULL)
+			{
+				print_cmd_err(info);
+			}
+			else
+			{	process(info);
+				free(info->path);
+			}
+		}	free_args(info->args);
+	}
+}
+
+/**
+ * path_tok - Find the full path of a command.
+ * @info: A pointer to the hsh structure.
+ *
+ * Return: The full path of the command or NULL if not found.
+ */
+char *path_tok(hsh *info)
+{
+	char *pass, *path = NULL, *command;
+	char *token, *name;
+	int i = my_strlen(info->args[0]);
+	struct stat st;
+
+	path = _getenv("PATH", _env(info));
+	pass = my_strdup(path);
+	token = sstrtok(pass, ":");
+	if (path == NULL)
+	{
+		return (NULL);
+	}
+
+	command = my_strdup(info->args[0]);
+	if (stat(command, &st) == 0)
+	{
+		return (command);
+	}
+	while (token != NULL)
+	{
+		name = malloc(sizeof(char) * (my_strlen(token) + 2 + i));
+		if (name == NULL)
+		{
+			perror("Error allocating space\n");
+			return (NULL);
+		}
+		my_strcpy(name, token);
+		my_strcat(name, "/");
+		my_strcat(name, command);
+		if (stat(name, &st) == 0)
+		{
+			free(pass);
+			free(command);
+			return (name);
+		}
+		token = sstrtok(NULL, ":");
+		free(name);
+	}
+	return (token);
+}
+
---
+++ get_env.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+
+/**
+ * _env - Create an array of environment variables.
+ * @info: A pointer to the hsh structure.
+ *
+ * Return: An array of environment variables or NULL on failure.
+ */
+char **_env(hsh *info)
+{
+	char **temp;
+	int i = 0, j = 0;
+	list_t *ptr = NULL;
+
+	ptr = info->environ;
+	temp = malloc(sizeof(char *) * (info->node_len + 1));
+	if (temp == NULL)
+		return (NULL);
+
+	while (ptr)
+	{
+		temp[i] = malloc(sizeof(char) * (my_strlen(ptr->str) + 1));
+		if (temp[i] == NULL)
+		{
+			while (temp[j])
+			{
+				free(temp[j]);
+				j++;
+			}
+			free(temp);
+			return (NULL);
+		}
+
+		my_strcpy(temp[i], ptr->str);
+		ptr = ptr->next;
+		i++;
+	}
+
+	temp[i] = NULL;
+	info->env = temp;
+	return (temp);
+}
+
+/**
+ * _getenv - Get the value of an environment variable.
+ * @path: The name of the environment variable to find.
+ * @envirn: An array of environment variables.
+ *
+ * Return: The value of the environment variable or NULL if not found.
+ */
+char *_getenv(char *path, char **envirn)
+{
+	char *env_cp;
+	int i;
+	char *var, *val = NULL;
+
+	if (envirn == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; envirn[i] != NULL; i++)
+	{
+		env_cp = my_strdup(envirn[i]);
+
+		var = sstrtok(env_cp, "=");
+		if (my_strcmp(var, path) == 0)
+		{
+			val = sstrtok(NULL, "=");
+			return (val);
+		}
+		free(env_cp);
+	}
+	return (val);
+}
+
