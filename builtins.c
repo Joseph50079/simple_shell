a1a1ab1a0cc142938d8900f6c357df27874f4a79
Joseph50079 <josephogbah2019@gmail.com>
2023-09-20 14:20:14 +0000

    now working

--- builtins.c
+++ builtins.c
@@ -11,16 +11,16 @@ int print_env(hsh *info)
 	int i = 0;
 	char *env;
 
-	info->env = _env(info);
+	/**info->env = _env(info);*/
 
 	if (info->env == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 
-	while (info->env[i])
+	while (environ)
 	{
-		env = info->env[i];
+		env = environ[i];
 		print_str(env, 1);
 		_sputchar('\n');
 		i++;
@@ -39,28 +39,21 @@ int exit_fun(hsh *info)
 	int status = 0;
 	char *at;
 
-	if (info->args != NULL)
+	if (info->args[1])
 	{
-		at = info->args[1];
-		status = atoi(at);
-		free(info->arg);
-		free_args(info->args);
-	}
+		if (info->args != NULL)
+		{
+			at = info->args[1];
+			status = atoi(at);
+			free(info->arg);
+			info->path = NULL;
+		}
 
-	if (info->environ != NULL)
-	{
-		free_list(info->environ);
-		free(info->env);
-	}
-
-	if (info->av != NULL)
-	{
-		free_args(info->av);
-	}
-
-	if (info->path != NULL)
-	{
-		free(info->path);
+		if (info->environ != NULL)
+		{
+			free_list(info->environ);
+			free(info->env);
+		}
 	}
 
 	exit(status);
